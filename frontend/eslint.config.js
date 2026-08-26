809e3d8a7d26765c0d798a73e3cc2a823d05ab85
randspy <randspy@pm.me>
2026-08-26 19:29:10 +0200

    chore: mutation testing - frontend (#12)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,7 +6,16 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'playwright-report', 'test-results', 'src/api/generated'] },
+  {
+    ignores: [
+      'dist',
+      'coverage',
+      'reports',
+      'playwright-report',
+      'test-results',
+      'src/api/generated',
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   query.configs['flat/recommended'],
